5747ea336a3d37d2d9f694ebb79150bc7ff8977b
Dmels <136374991+Edwinale20@users.noreply.github.com>
2026-05-09 19:51:21 -0600

    Update Habilitados.py

--- pages/Habilitados.py
+++ pages/Habilitados.py
@@ -13,7 +13,7 @@ import io
 import tempfile
 #---------------------------------------------------------------------------------------- 
 st.set_page_config(page_title="Habilitados Marca Propia", page_icon="🏪", layout="wide", initial_sidebar_state="expanded")
-st.title("📊 Reporte de Habilitados | Marca Propia 🏪")
+st.title("🟢 Reporte de Habilitados | Marca Propia 🏪")
 st.markdown("✅ Arrastra aquí el archivo de habilitados, por división")
 st.markdown("🔐 Esta app no guarda datos en la nube o en caché. Si deseas reiniciar todo solo da refresh a la página")
 kpi_top = st.container()
